be0dbc5ee6f361cb9607ce3ba0c64623cc65d88b
imiguel <miguel.rosa17@gmail.com>
2015-11-06 16:27:29 +0000

    Imports

--- mainWindows.py
+++ mainWindows.py
@@ -1,13 +1,5 @@
 from tkinter import *
 from serial import *
-
-
-
-def readSerialPort():
-    serialPort = serial.Serial('COM3', 9600)
-
-#ser = serial.Serial('COM3', 9600) #serial.Serial('porta', 9600)
-#s = ser.read(1000)
-#print(s)
+from wx import *
 
 
