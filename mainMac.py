6018cd07562341050686b731e52da847b32948ed
imiguel <miguel.rosa17@gmail.com>
2015-11-07 15:06:32 +0000

    Mac

--- mainMac.py
+++ mainMac.py
@@ -1,1 +1,39 @@
-from tkinter import *
+import wx
+import os
+
+class mainWindow(wx.Frame):
+    #criar a estrutura da janela, menu, botoes, etc
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title="Medidor", size=(700,500))
+        self.CreateStatusBar() # A StatusBar in the bottom of the window
+
+        #criar um menu
+        fileMenu = wx.Menu()
+
+        #criar os sub-menus do menu de cima
+        menuAbout = fileMenu.Append(wx.ID_ABOUT, "&Sobre"," Sobre o programa")
+
+
+        #cria a barra de menus
+        menuBar = wx.MenuBar()
+        menuBar.Append(fileMenu,"&Ficheiro") #adiciona o "filemenu" ao MenuBar
+        self.SetMenuBar(menuBar)  #adiciona o MenuBar a janela
+
+        #cria os eventos nos botoes do menu
+        self.Bind(wx.EVT_MENU, self.onClickSobre, menuAbout)
+
+
+        #finalmente mostra todas as opcoes
+        self.Show(True)
+
+
+    #Metodos dos clicks dos botoes no menu
+    def onClickSobre(self,e):
+        dlg = wx.MessageDialog( self, "Medidor de Densidade Liquida\nAutor: Miguel Rosa\nESTiG - IPB\nNovembro/Dezembro 2015");
+        dlg.ShowModal() #mostra a caixa dialogo
+        dlg.Destroy() #apos terminar, destroi
+
+
+aplicacao = wx.App(False)
+frame = mainWindow(None, "MEDIDOR")
+aplicacao.MainLoop()
