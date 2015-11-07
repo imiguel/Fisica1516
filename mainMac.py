9cd6f3fbcc003a0d8234ea0f8e57378495be6940
imiguel <miguel.rosa17@gmail.com>
2015-11-07 15:16:52 +0000

    Mac

--- mainMac.py
+++ mainMac.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import wx
 import os
 
@@ -11,17 +14,18 @@ class mainWindow(wx.Frame):
         fileMenu = wx.Menu()
 
         #criar os sub-menus do menu de cima
-        menuAbout = fileMenu.Append(wx.ID_ABOUT, "&Sobre"," Sobre o programa")
-
+        menuAbout = fileMenu.Append(wx.ID_ABOUT, "&Sobre", "Sobre o programa")
+        menuExit = fileMenu.Append(wx.ID_EXIT,"&Sair"," Termina o programa")
 
         #cria a barra de menus
         menuBar = wx.MenuBar()
         menuBar.Append(fileMenu,"&Ficheiro") #adiciona o "filemenu" ao MenuBar
         self.SetMenuBar(menuBar)  #adiciona o MenuBar a janela
 
+
         #cria os eventos nos botoes do menu
         self.Bind(wx.EVT_MENU, self.onClickSobre, menuAbout)
-
+        self.Bind(wx.EVT_MENU, self.OnClickSair, menuExit)
 
         #finalmente mostra todas as opcoes
         self.Show(True)
@@ -32,6 +36,9 @@ class mainWindow(wx.Frame):
         dlg = wx.MessageDialog( self, "Medidor de Densidade Liquida\nAutor: Miguel Rosa\nESTiG - IPB\nNovembro/Dezembro 2015");
         dlg.ShowModal() #mostra a caixa dialogo
         dlg.Destroy() #apos terminar, destroi
+
+    def OnClickSair(self,e):
+        self.Close(True)  # Close the frame.
 
 
 aplicacao = wx.App(False)
